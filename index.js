5115806c41fc3b829e445f1d841e8d598077ac24
Maria Cristina Ordonez <crisordonez44@gmail.com>
2022-10-04 12:02:40 -0500

    commit tree c1ed0ee34414fcfc4cc9cd9a1c9e4e942245e378

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching (arrayOfDriverNames, driverWeAreLookingFor){
+    return arrayOfDriverNames.filter ((driverName) => driverName.toLowerCase ().includes(driverWeAreLookingFor.toLowerCase()));
+}
+
+//const myArray =["Luis", "Luis","Cris", "Bob", "Sofia", "Tomas" ]
+
+//console.log (findMatching(myArray, "LUis"))
+
+function fuzzyMatch (arrayOfDriversNames, firstTwoLetters){
+    return arrayOfDriversNames.filter ((driverName) => driverName.startsWith (firstTwoLetters));
+}
+
+
+//const myArray =["Luis", "Luis","Cris", "Bob", "Sofia", "Tomas" ]
+//console.log (fuzzyMatch(myArray, "Lu"))
+
+function matchName(arrayOfDriversObjects,matchString ){
+    return arrayOfDriversObjects.filter ((driverName) => driverName.name  === matchString);
+}
